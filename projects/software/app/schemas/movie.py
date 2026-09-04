0fd7f919eecac9d19ed1b419ec98fafc25d258d4
Roya Mohammadi <royamohammadi13832004mh@gmail.com>
2026-09-04 23:32:11 +0330

    feat: add movie and showtime endpoints, wire up routers

--- projects/software/app/schemas/movie.py
+++ projects/software/app/schemas/movie.py
@@ -1,10 +1,33 @@
+from datetime import datetime
+
 from pydantic import BaseModel
+
 
 class MovieCreate(BaseModel):
     title: str
     duration: int
 
-class MovieOut(BaseModel):
+
+class MovieResponse(BaseModel):
     id: int
     title: str
-    duration: int 
+    duration: int
+
+    class Config:
+        from_attributes = True
+
+
+class ShowtimeCreate(BaseModel):
+    movie_id: int
+    hall_id: int
+    start_time: datetime
+
+
+class ShowtimeResponse(BaseModel):
+    id: int
+    movie_id: int
+    hall_id: int
+    start_time: datetime
+
+    class Config:
+        from_attributes = True
